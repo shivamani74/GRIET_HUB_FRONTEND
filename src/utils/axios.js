aae546780e5ac06a6f4ecc2af17973bc43670c81
shivamani74 <shivamaniboina@gmail.com>
2026-03-02 11:20:59 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/api",  
+  baseURL: "https://griet-hub-backend.onrender.com/api",  
 });
 
 instance.interceptors.request.use((config) => {
